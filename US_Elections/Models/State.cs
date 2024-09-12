85ced28ee29b05ade83fc3db1d91c7236b51b8cd
Hristiana Hristova <HHristova@infragistics.com>
2024-09-12 16:13:25 +0300

    creating models and data file

---
+++ US_Elections/Models/State.cs
@@ -0,0 +1,8 @@
+﻿namespace US_Elections.Models
+{
+    public class State
+    {
+        public string S { get; set; } // State abbreviation
+        public List<StateResult> R { get; set; } // Results
+    }
+}
